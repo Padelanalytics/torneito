d7feb289d3259b6010767a43f51789bd5a181048
Francisco Revilla <francisco.revilla@infarm.com>
2023-05-23 14:28:17 +0200

    Methods add, remove and addBulk are now passed by reference.

--- model/games.go
+++ model/games.go
@@ -15,29 +15,30 @@ func (gs Games) Less(i, j int) bool {
 	return gs[i].Compare(gs[j]) < 0
 }
 
-func (gs Games) Add(g Game) Games {
-	gs = append(gs, g)
-	sort.Sort(Games(gs))
-	return gs
+func (gs *Games) Add(g Game) {
+	copy := *gs
+	*gs = append(copy, g)
+	sort.Sort(Games(*gs))
 }
 
-func (gs Games) AddBulk(games []Game) Games {
-	gs = append(gs, games[0:]...)
-	sort.Sort(Games(gs))
-	return gs
+func (gs *Games) AddBulk(games []Game) {
+	copy := *gs
+	*gs = append(copy, games[0:]...)
+	sort.Sort(Games(*gs))
 }
 
-func (gs Games) Remove(index int) Games {
-	if index < 0 || index >= len(gs) {
-		return gs
+func (gs *Games) Remove(index int) {
+	if index < 0 || index >= len(*gs) {
+		return
 	}
-	return append(gs[:index], gs[index+1:]...)
+	copy := *gs
+	*gs = append(copy[:index], copy[index+1:]...)
 }
 
 func (gs Games) FromRecords(records [][]string) Games {
 	for _, r := range records {
 		fmt.Println(r)
-		gs = gs.Add(Game{Country: r[0], Name: r[1]})
+		gs.Add(Game{Country: r[0], Name: r[1]})
 	}
 	return gs
 }
