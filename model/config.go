b3a0838c455b6515eb1511feb0a80fa7ca2a2984
Francisco Revilla <francisco.revilla@infarm.com>
2023-07-26 09:31:10 +0200

    Add a config file to associate csv columns with Game fields, and to help sorting rounds and categories.

---
+++ model/config.go
@@ -0,0 +1,47 @@
+package model
+
+var cR = map[string]int{
+	"country":  0,
+	"name":     1,
+	"serie":    2,
+	"division": 3,
+	"date":     4,
+	"round":    7,
+	"category": 8,
+	"teams":    9,
+	"p1_last":  12,
+	"p1_first": 13,
+	"p2_last":  14,
+	"p2_first": 15,
+	"p3_last":  16,
+	"p3_first": 17,
+	"p4_last":  18,
+	"p4_first": 19,
+	"sets":     20,
+}
+
+var cRound = map[string]int{
+	"KO1":  0,
+	"KO2":  1,
+	"KO3":  2,
+	"KO4":  3,
+	"KO5":  4,
+	"KO6":  5,
+	"KO7":  6,
+	"KO8":  7,
+	"KO9":  8,
+	"KO10": 9,
+	"KO11": 10,
+	"KO12": 11,
+	"KO13": 12,
+	"KO14": 13,
+	"KO15": 14,
+	"KO16": 15,
+	"KO32": 16,
+}
+
+var cCategory = map[string]int{
+	"Preprevia": 0,
+	"Previa":    1,
+	"Gold":      2,
+}
