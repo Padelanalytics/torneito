6fac59c5f950bd2b40b7177d98b0cb0eea25fe8d
Francisco Revilla <francisco.revilla@infarm.com>
2023-07-27 19:32:38 +0200

    Test IsTournament method.

--- model/game_test.go
+++ model/game_test.go
@@ -1,19 +1,20 @@
-package model
+package model_test
 
 import (
 	"testing"
 
+	m "github.com/paconte/torneito/model"
 	"github.com/stretchr/testify/assert"
 )
 
 var date = "10.20.2020"
-var g1 = Game{Country: "GERMANY", Name: "Berlin", Date: date}
-var g2 = Game{Country: "GERMANY", Name: "Muenchen", Date: date}
-var g3 = Game{Country: "GERMANY", Name: "Berlin", Date: date}
-var g4 = Game{Country: "SPAIN", Name: "Berlin", Date: date}
-var g5 = Game{Country: "GERMANY", Name: "Berlin", Date: "20.10.2021"}
+var g1 = m.Game{Country: "GERMANY", Name: "Berlin Open", Date: date}
+var g2 = m.Game{Country: "GERMANY", Name: "Muenchen Open", Date: date}
+var g3 = m.Game{Country: "GERMANY", Name: "Berlin Open", Date: date}
+var g4 = m.Game{Country: "SPAIN", Name: "Berlin Open", Date: date}
+var g5 = m.Game{Country: "GERMANY", Name: "Berlin Open", Date: "20.10.2021"}
 
-func TestCompare(t *testing.T) {
+func TestGCompare(t *testing.T) {
 	assert.Equal(t, 0, g1.Compare(g3))
 	assert.Equal(t, -1, g1.Compare(g2))
 	assert.Equal(t, +1, g2.Compare(g1))
@@ -21,15 +22,15 @@ func TestCompare(t *testing.T) {
 	assert.Equal(t, -1, g1.Compare(g5))
 }
 
-func TestIsTournament(t *testing.T) {
-	game1 := Game{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-1000", Division: "MO", Date: date}
-	game2 := Game{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-1000", Division: "WO", Date: date}
-	game3 := Game{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-500", Division: "MO", Date: date}
-	game4 := Game{Country: "GERMANY", Name: "Berlin Open II", Serie: "GPS-1000", Division: "MO", Date: date}
-	game5 := Game{Country: "SPAIN", Name: "Berlin Open", Serie: "GPS-1000", Division: "MO", Date: date}
-	game6 := Game{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-1000", Division: "MO", Date: "20.10.2021"}
+func TestGIsTournament(t *testing.T) {
+	game1 := m.Game{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-1000", Division: "MO", Date: date}
+	game2 := m.Game{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-1000", Division: "WO", Date: date}
+	game3 := m.Game{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-500", Division: "MO", Date: date}
+	game4 := m.Game{Country: "GERMANY", Name: "Berlin Open II", Serie: "GPS-1000", Division: "MO", Date: date}
+	game5 := m.Game{Country: "SPAIN", Name: "Berlin Open", Serie: "GPS-1000", Division: "MO", Date: date}
+	game6 := m.Game{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-1000", Division: "MO", Date: "20.10.2021"}
 
-	tournament := Tournament{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-1000", Division: "MO"}
+	tournament := m.Tournament{Country: "GERMANY", Name: "Berlin Open", Serie: "GPS-1000", Division: "MO"}
 
 	assert.Equal(t, true, game1.IsTournament(tournament))
 	assert.Equal(t, false, game2.IsTournament(tournament))
